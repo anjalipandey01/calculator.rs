277d8b8b622619f420cc460867ec873d7b636feb
anjalipandey01 <142039643+anjalipandey01@users.noreply.github.com>
2023-08-17 20:39:38 +0530

    Add files via upload

---
+++ calculator.rs
@@ -0,0 +1,26 @@
+fn main(){
+    let num1=10.0;
+    let num2=5.0;
+
+    println!("Addition:{}",add(&num1,&num2));
+    println!("Subtraction:{}",subtract(&num1,&num2));
+    println!("Multiplication:{}",multiply(&num1,&num2));
+    println!("Division:{}",divide(&num1,&num2));
+}
+fn add(a: &f64,b: &f64) -> f64 {
+    a+b
+}
+fn subtract(a: &f64,b: &f64) -> f64 {
+    a-b
+}
+fn multiply(a: &f64,b: &f64) -> f64 {
+    a*b
+}
+fn divide(a: &f64,b: &f64) -> f64 {
+    if *b != 0.0{
+        a/b
+    }
+    else{
+        panic!("cannot divide by zero");
+    }
+}
